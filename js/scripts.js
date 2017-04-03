8d76f18e8608a7fa4798b8378e8470ba9b36e2fe
Nicole Santamaria and Spencer Ruiz <nicole@>
2017-04-03 13:38:52 -0700

    update scripts.js to allow city-country links to show individual information on click.

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,11 @@ function Landmark (landmark) {
 };
 
 Location.prototype.locationName = function() {
-  return `${this.cityName} ${this.countryName}`;
+  return `${this.cityName}, ${this.countryName}`;
+};
+
+Landmark.prototype.landmarkName = function (){
+  return this.landmark;
 };
 
 function resetFields() {
@@ -27,9 +31,11 @@ function resetFields() {
 $(document).ready(function(){
 
   $("#add-landmark").click(function(){
-    $("#landmarks").append(`<div class="form-group">
-                              <label for="landmarks">Landmark</label>
-                              <input class="form-control new-landmark" type="text">
+    $("#landmarks").append(`<div class="landmark-group">
+                              <div class="form-group">
+                                <label for="landmarks">Landmark</label>
+                                <input class="form-control new-landmark" type="text">
+                              </div>
                             </div>`)
   });
 
@@ -40,27 +46,24 @@ $(document).ready(function(){
     var year = $("#year-input").val();
     var notes = $("#notes-input").val();
     let location = new Location (cityName, countryName, year, notes);
-    $("#location-list").append("<li>" + location.cityName + " " + location.countryName + "</li>")
-    $(".new-landmark").each(function(){
+//reference the landmark-group class for the each function because there are many landmarks that will be created in the div #landmarks
+    $(".landmark-group").each(function(){
       var landmark = $(this).find("input.new-landmark").val();
       var newLandmark = new Landmark (landmark);
       location.landmarks.push(newLandmark)
     });
-
-    $("#location-list").last().click(function(){
+    $("#location-list").append("<li><span class='city-country'>" + location.cityName + " " + location.countryName + "</span></li>")
+//create span class above and reference class below on click function to show each objects information
+    $(".city-country").last().click(function(){
       $(".location-info").show();
       $("#locationNameDisplay").text(location.locationName());
       $("#cityDisplay").text(location.cityName);
       $("#countryDisplay").text(location.countryName);
       $("#landmarksDisplay").empty();
       location.landmarks.forEach(function(landmark){
-        $("#landmarksDisplay").append(`<li>${landmark.landmarks}</li>`)
+        $("#landmarksDisplay").append(`<li>${landmark.landmarkName()}</li>`)
       });
-
     });
-
+    resetFields();
   });
-
-
-
 });
